236a613e51d430d566e8cce5751cf63c1f7f3ce7
Daniel Kamanga <155548093+TaffyDevs11@users.noreply.github.com>
2025-03-19 08:54:23 +0100

    Add files via upload

---
+++ online/footer.php
@@ -0,0 +1,8 @@
+<center>
+		<footer>
+		
+		<p>TaffyDevs High School 2023</p>
+			<!-- <p>Programmed by: Tafadzwa Kamanga</p> -->
+		</footer>
+</center>
+
---
+++ online/login_form.php
@@ -0,0 +1,44 @@
+			<form id="login_form1" class="form-signin" method="post">
+						<h2 class="form-signin-heading"><i class="icon-lock"></i> Sign in</h2>
+						<h3 class="form-signin-heading"><i class="icon-lock"></i> Sign in as Teacher or Student</h3>
+						<input type="text" class="input-block-level" id="username" name="username" placeholder="Username" required>
+						<input type="password" class="input-block-level" id="password" name="password" placeholder="Password" required>
+						<button data-placement="right" title="Click Here to Sign In" id="signin" name="login" class="btn btn-info" type="submit"><i class="icon-signin icon-large"></i> Sign in</button>
+														<script type="text/javascript">
+														$(document).ready(function(){
+															$('#signin').tooltip('show');
+															$('#signin').tooltip('hide');
+														});
+														</script>	
+						<button data-placement="left" title="Back to Home" class="btn btn-info" href="mobirise/index.php"><i class="icon-home icon-large"></i> Home</button>	
+			</form>
+						<script>
+						jQuery(document).ready(function(){
+						jQuery("#login_form1").submit(function(e){
+								e.preventDefault();
+								var formData = jQuery(this).serialize();
+								$.ajax({
+									type: "POST",
+									url: "login.php",
+									data: formData,
+									success: function(html){
+									if(html=='true')
+									{
+									$.jGrowl("Loading File Please Wait......", { sticky: true });
+									$.jGrowl("Welcome to TaffyDevs Learning Management System", { header: 'Access Granted' });
+									var delay = 1000;
+										setTimeout(function(){ window.location = 'dasboard_teacher.php'  }, delay);  
+									}else if (html == 'true_student'){
+										$.jGrowl("Welcome to TaffyDevs Learning Management System", { header: 'Access Granted' });
+									var delay = 1000;
+										setTimeout(function(){ window.location = 'student_notification.php'  }, delay);  
+									}else
+									{
+									$.jGrowl("Please Check your username and Password", { header: 'Login Failed' });
+									}
+									}
+								});
+								return false;
+							});
+						});
+						</script>
