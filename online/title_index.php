c6be86706daa97dd2ebfefbb9041a4ed6f0937ec
Daniel Kamanga <155548093+TaffyDevs11@users.noreply.github.com>
2025-03-19 08:59:39 +0100

    Add files via upload

---
+++ online/title_index.php
@@ -0,0 +1,37 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="shs">TaffyDevs High School</p>
+							<h3>
+
+							<p>Online Classroom</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>TaffyDevs High School:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Science and Technology</p>
+								</div>		
+						</div>		
+				</div>
